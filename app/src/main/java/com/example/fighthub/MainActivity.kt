27b4289bc4ae1bd1354d0ef03d6a73940d5af80e
Illmati <andrea.illuminati125gmail.com>
2026-04-17 11:34:55 +0200

    navabr aggiustata. tante cose

--- app/src/main/java/com/example/fighthub/MainActivity.kt
+++ app/src/main/java/com/example/fighthub/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +20,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_main_container, MainFragmentMenu())
+                .commit()
         }
 
         // Tasto indietro per andare nel login, non nella registrazione.
@@ -34,7 +42,66 @@ class MainActivity : AppCompatActivity() {
                 finish() // Chiude MainActivity
             }
         }
+
+        //navbar
+        val navBar = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navBar.selectedItemId = R.id.nav_fight
+        navBar.setOnItemSelectedListener { item ->
+           when(item.itemId) {
+                R.id.nav_chat -> {
+                    navigaAllaChat()
+                    true
+                }
+                R.id.nav_fight -> {
+                    navigaAlMenu()
+                    true
+                }
+                R.id.nav_profilo -> {
+                    navigaAlProfilo()
+                    true
+                }
+                else -> false
+            }
+
+        }
+
         // Aggiunge il callback al dispatcher
         onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    fun navigaAllaChat() {
+        supportFragmentManager.beginTransaction()
+            // Animazione: entra da destra, esce a sinistra
+            .setCustomAnimations(
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right
+            )
+            .replace(R.id.fragment_main_container, MainFragmentChat()) // Carica il secondo fragment
+            .addToBackStack(null) // Permette di tornare indietro col tasto back
+            .commit()
+    }
+
+    fun navigaAlProfilo() {
+        supportFragmentManager.beginTransaction()
+            // Animazione: entra da destra, esce a sinistra
+            .setCustomAnimations(
+                android.R.anim.slide_out_right,
+                android.R.anim.slide_in_left
+            )
+            .replace(R.id.fragment_main_container, MainFragmentProfiloUtente()) // Carica il secondo fragment
+            .addToBackStack(null) // Permette di tornare indietro col tasto back
+            .commit()
+    }
+
+    fun navigaAlMenu() {
+        supportFragmentManager.beginTransaction()
+            // Animazione: entra da destra, esce a sinistra
+            .setCustomAnimations(
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right
+            )
+            .replace(R.id.fragment_main_container, MainFragmentMenu()) // Carica il secondo fragment
+            .addToBackStack(null) // Permette di tornare indietro col tasto back
+            .commit()
     }
 }
