2e73efc52e03c94e4dcfd67f477309901f314981
Diego <capeccidiego04@gmail.com>
2026-05-08 12:04:19 +0200

    piccola sugli import

--- app/src/main/java/com/example/fighthub/MainFragmentChatUtente.kt
+++ app/src/main/java/com/example/fighthub/MainFragmentChatUtente.kt
@@ -1,13 +1,12 @@
 package com.example.fighthub
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.compose.runtime.savedinstancestate.savedInstanceState
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
